312e6ac895b84e78cc1f9ed856b09f2f3ff58413
aparkening <aaron.parkening@gmail.com>
2019-08-05 14:31:38 -0400

    Add gitignore and set session_secret to variable.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
 
-  enable :sessions
-  set :session_secret, "d3c6b77d15ac10d78c5e0d4797b159614f162810edb34cb52f330cd7ee2434729159e785595d074857c57794d38f34ddb45719a1ed94206aa54631ed6a5ad16e"
-  set :views, Proc.new { File.join(root, "../views/") }
+  configure do 
+    set :views, 'app/views'
+    set :public_folder, 'public'
+
+    enable :sessions
+    set :session_secret, SESSION_SECRET
+  end
+  # BEFORE PRODUCTION: set environment variable secret
 
   get '/' do
     erb :index
