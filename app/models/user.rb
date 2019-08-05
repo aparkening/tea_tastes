5a89b03d7fa7ebafc1141b91b74bcfe9cb3c640f
aparkening <aaron.parkening@gmail.com>
2019-08-05 15:19:00 -0400

    Add validates to user.rb. Comment out later confirmation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   include Slugify::InstanceMethods
 
   has_many :notes
+
+  validates :username, presence: true
+  validates :password, length: { minimum: 10 }
+  #  validates :password, length: { minimum: 10 }, confirmation: true
 end
