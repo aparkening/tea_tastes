a885db4862d6d0015a00a021a5f1d4a505e35dc4
aparkening <aaron.parkening@gmail.com>
2019-08-09 12:41:57 -0400

    Add user edit template. Add edit route and patch. Changing username works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,45 @@
 # Control user pages
 class UsersController < ApplicationController
 
+#### Edit
+  # Show edit form if user has permission
+  get '/users/:slug/edit' do 
+    # Ensure user can take this action
+    authorize
+
+    @user = User.find_by_slug(params[:slug]) 
+
+   # Ensure only owner can edit
+    authorize_user(@user)
+
+    erb :'/users/edit'  
+  end
+
+  # Update in database
+  patch '/users/:slug' do  
+    # Ensure user can take this action
+    authorize
+
+    if params[:user]["username"].empty?
+      flash[:message] = ["Fields are missing data. Please submit again."]
+      flash[:type] = "error"
+      redirect "/users/#{params[:slug]}/edit"
+    else 
+      user = User.find_by_slug(params[:slug]) 
+
+      # Ensure only owner can edit
+      authorize_user(user)
+
+      user.update(params[:user])
+
+      flash[:message] = ["Success! Note updated."]
+      flash[:type] = "success"
+        
+      redirect "/users/#{user.slug}"
+    end
+  end
+
+  #### Display
   # Display user show page
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
