5e1ce3dd94cce00932e1666ee7b28681392dc904
aparkening <aaron.parkening@gmail.com>
2019-08-09 12:25:04 -0400

    Remove on create for username validation to ensure edits to username also are unique.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,12 @@ class User < ActiveRecord::Base
   has_many :notes
 
   # validates :name, presence: true, on: :create
-  validates :username, presence: true, uniqueness: true, on: :create
+  validates :username, presence: true, uniqueness: true
 
   validates :password, presence: true, on: :create
   validates :password, length: { minimum: 6 }, confirmation: true, unless: ->(u){ u.password.blank? }
-  validates :password_confirmation, presence: true
+  validates :password_confirmation, presence: true, on: :create
+  # validates :password_confirmation, unless: ->(u){ u.password.blank? }
 
   def slug_name
     self.username
