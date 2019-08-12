997021344e8835ce5125b35dd5a70e67413e8228
aparkening <aaron.parkening@gmail.com>
2019-08-12 12:41:39 -0400

    Remove TeasShops join from Tea and Shop models.

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,13 +2,16 @@ class Shop < ActiveRecord::Base
   extend Slugify::ClassMethods
   include Slugify::InstanceMethods
 
-  has_many :teas_shops
-  has_many :teas, through: :notes
+  # has_many :teas_shops
 
-  has_many :teas, through: :teas_shops
-  # has_many :notes, through: :teas
   has_many :notes_shops
   has_many :notes, through: :notes_shops
+  has_many :teas, through: :notes
+
+  # has_many :teas, through: :teas_shops
+  # has_many :notes, through: :teas
+  # has_many :notes_shops
+  # has_many :notes, through: :notes_shops
 
 
   validates :name, presence: true, uniqueness: true
--- app/models/tea.rb
+++ app/models/tea.rb
@@ -4,8 +4,9 @@ class Tea < ActiveRecord::Base
 
   has_many :notes
   has_many :users, through: :notes
-  has_many :teas_shops
-  has_many :shops, through: :teas_shops
+  has_many :shops, through: :notes
+  # has_many :teas_shops
+  # has_many :shops, through: :teas_shops
 
   validates :name, presence: true, uniqueness: true
   validates :category, presence: true  
