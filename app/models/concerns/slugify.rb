da3ccc842eecb7199f3afbafa30cb0fe421e762f
aparkening <aaron.parkening@gmail.com>
2019-08-15 17:44:16 -0400

    Update slugify to add id as way of making slugs truly unique.

--- app/models/concerns/slugify.rb
+++ app/models/concerns/slugify.rb
@@ -3,14 +3,18 @@ module Slugify
   module InstanceMethods
     # Strip special characters and replace with "-"
     def slug 
-      slug_name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+      # Original operation: slug_name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+
+      # Make more unique now that sanitize removes characters
+      slugged = slug_name.downcase.strip.gsub(' ', '-').gsub(/[^\w_-]/, '') + "_" + self.id.to_s
+      return slugged
     end
   end
 
   module ClassMethods
     # Return object from database based on slug
     def find_by_slug(slug)
-      self.all.find{ |obj| obj.slug == slug}
+      self.all.find { |obj| obj.slug == slug }
     end
   end
 
