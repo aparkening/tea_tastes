51fda4965951acb7593268fa88c913f32866a516
aparkening <aaron.parkening@gmail.com>
2019-08-26 16:39:38 -0400

    Remove extraneous models.

--- app/models/note_shop.rb
+++
@@ -1,7 +0,0 @@
-class NotesShop < ActiveRecord::Base
-  extend Slugify::ClassMethods
-  include Slugify::InstanceMethods
-
-  belongs_to :note
-  belongs_to :shop
-end
--- app/models/tea_shop.rb
+++
@@ -1,7 +0,0 @@
-class TeasShop < ActiveRecord::Base
-  extend Slugify::ClassMethods
-  include Slugify::InstanceMethods
-
-  belongs_to :tea
-  belongs_to :shop
-end
